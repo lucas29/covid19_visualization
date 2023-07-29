0055ba078bce1a4a65e2914c77079dae8d0642f5
Tatsu Iriya <lucas29liao@gmail.com>
2023-07-29 15:21:21 +0900

    refactored

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ const [LINE_CHART_TITLE, WORLD_MAP_TITLE] = [
 
 const COUNTRY_NAME_MAP = {
     "UNITED STATES OF AMERICA": "US",
-    "Myanmar": "Burma"
+    "Myanmar": "Burma",
+    "North Korea": "Korea North",
+    "South Korea": "Korea South"
     // Add more mappings as needed
 };
 
@@ -52,74 +54,131 @@ d3.select('#next').on('click', () => {
     displayScene(index);
 });
 
+// 1. Separate Data Parsing
+function parseLineChartData(data) {
+    const parsedData = data.flatMap(d => Object.entries(d)
+        .filter(([key]) => !["Province/State", "Country/Region", "Lat", "Long"].includes(key))
+        .map(([key, value]) => ({
+            date: d3.timeParse("%m/%d/%y")(key),
+            cases: +value
+        }))
+    );
+
+    const dataByCountry = data.reduce((acc, d) => {
+        if (!acc[d["Country/Region"]]) {
+            acc[d["Country/Region"]] = [];
+        }
+        acc[d["Country/Region"]].push(d);
+        return acc;
+    }, {});
+
+    return { parsedData, dataByCountry };
+}
+
 d3.csv("confirmed.csv").then(data => {
-  const parsedData = data.flatMap(d => Object.entries(d).filter(([key]) => !["Province/State", "Country/Region", "Lat", "Long"].includes(key)).map(([key, value]) => ({
+
+// 2. Render Line Chart
+function renderLineChart({ parsedData, dataByCountry }) {
+    const svg = d3.select("#line-chart-container").append("svg")
+        .attr("width", WIDTH + MARGIN.left + MARGIN.right)
+        .attr("height", HEIGHT + MARGIN.top + MARGIN.bottom)
+        .append("g")
+        .attr("transform", `translate(${MARGIN.left},${MARGIN.top})`);
+
+    // Add X axis
+    var x = d3.scaleTime()
+        .domain(d3.extent(parsedData, function(d) { return d.date; }))
+        .range([0, WIDTH]);
+    svg.append("g")
+        .attr("transform", "translate(0," + HEIGHT + ")")
+        .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(parsedData, function(d) { return +d.cases; })])
+        .range([HEIGHT, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+const allCountries = Object.keys(dataByCountry);
+//console.log(allCountries);
+const colors = d3.scaleOrdinal(d3.schemeCategory10).domain(allCountries);
+
+Object.keys(dataByCountry).forEach(country => {
+    const countryData = dataByCountry[country].flatMap(d => Object.entries(d).filter(([key]) => !["Province/State", "Country/Region", "Lat", "Long"].includes(key)).map(([key, value]) => ({
     date: d3.timeParse("%m/%d/%y")(key),
     cases: +value
-  })));
-
-// Separate data by country
-  const dataByCountry = data.reduce((acc, d) => {
-    if (!acc[d["Country/Region"]]) {
-      acc[d["Country/Region"]] = [];
-    }
-    acc[d["Country/Region"]].push(d);
-    return acc;
-  }, {});
-
-  const allCountries = Object.keys(dataByCountry);
-  //console.log(allCountries);
-  const colors = d3.scaleOrdinal(d3.schemeCategory10).domain(allCountries);
-
-  const svg = d3.select("#line-chart-container").append("svg").attr("width", WIDTH + MARGIN.left + MARGIN.right).attr("height", HEIGHT + MARGIN.top + MARGIN.bottom).append("g").attr("transform", `translate(${MARGIN.left},${MARGIN.top})`);
-
-  // Add X axis
-  var x = d3.scaleTime()
-  .domain(d3.extent(parsedData, function(d) { return d.date; })).range([ 0, WIDTH ]);
-  svg.append("g")
-  .attr("transform", "translate(0," + HEIGHT + ")").call(d3.axisBottom(x));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-  .domain([0, d3.max(parsedData, function(d) { return +d.cases; })]).range([ HEIGHT, 0 ]);
-  svg.append("g").call(d3.axisLeft(y));
-
-  Object.keys(dataByCountry).forEach(country => {
-    const countryData = dataByCountry[country].flatMap(d => Object.entries(d).filter(([key]) => !["Province/State", "Country/Region", "Lat", "Long"].includes(key)).map(([key, value]) => ({
-      date: d3.timeParse("%m/%d/%y")(key),
-      cases: +value
     })));
     
     const totalCasesByDate = d3.rollups(countryData, v => d3.sum(v, d => d.cases), d => d.date);
 
     svg.append("path")
-      .datum(totalCasesByDate)
-      .attr("fill", "none")
-      .attr("stroke", colors(country))
-      .attr("stroke-width", 1.5)
-      .attr("class", "visible-line")  // Add a class for the visible line
-      .attr("data-country", country)  // Add a data attribute for the country name
-      .attr("d", d3.line()
+    .datum(totalCasesByDate)
+    .attr("fill", "none")
+    .attr("stroke", colors(country))
+    .attr("stroke-width", 1.5)
+    .attr("class", "visible-line")  // Add a class for the visible line
+    .attr("data-country", country)  // Add a data attribute for the country name
+    .attr("d", d3.line()
         .x(d => x(d[0]))
         .y(d => y(d[1]))
-      )
+    )
 
-      svg.append("path")
-      .datum(totalCasesByDate)
-      .attr("fill", "none")
-      .attr("stroke", "transparent")
-      .attr("stroke-width", 20)  // Adjust this width to determine how close the mouse has to be to the line to trigger the tooltip.
-      .attr("class", "invisible-line")  // Add a class for the invisible line
-      .attr("data-country", country)  // Add a data attribute for the country name
-      .attr("d", d3.line()
+    svg.append("path")
+    .datum(totalCasesByDate)
+    .attr("fill", "none")
+    .attr("stroke", "transparent")
+    .attr("stroke-width", 20)  // Adjust this width to determine how close the mouse has to be to the line to trigger the tooltip.
+    .attr("class", "invisible-line")  // Add a class for the invisible line
+    .attr("data-country", country)  // Add a data attribute for the country name
+    .attr("d", d3.line()
         .x(d => x(d[0]))
         .y(d => y(d[1]))
-      )
-      .on("mouseover", handleLineMouseOver)
-      .on("mousemove", (event, d) => handleLineMouseMove(event, d, country))
-      .on("mouseout", handleLineMouseOut);
+    )
+    .on("mouseover", handleLineMouseOver)
+    .on("mousemove", (event, d) => handleLineMouseMove(event, d, country))
+    .on("mouseout", handleLineMouseOut);
 
-  });
+    function handleLineMouseMove(event, d, country) {
+        const [xPos] = d3.pointer(event, this); 
+        const xDomain = x.invert(xPos);
+        const bisect = d3.bisector(d => d.date).left;
+        const index = bisect(parsedData, xDomain, 1);
+        const a = parsedData[index - 2];
+        const b = parsedData[index - 1];
+        const point = xDomain - a.date > b.date - xDomain ? b : a;
+    
+        const formattedDate = d3.timeFormat("%Y-%m-%d")(point.date);
+        const content = `<strong>Country: </strong>${country}<br>
+                        <strong>Date: </strong>${formattedDate}<br>
+                        <strong>Daily Confirmed Cases: </strong>${point.cases}`;
+        showTooltip(event, content);
+    }
+    
+    function handleLineMouseOver(d, i) {
+        const country = d3.select(this).attr("data-country");
+        const visibleLine = d3.select(`.visible-line[data-country="${country}"]`);
+    
+        visibleLine.style("cursor", "pointer");
+        visibleLine.style("stroke-width", 3);
+    }
+    
+    function handleLineMouseOut(d, i) {
+        hideTooltip();
+        
+        const country = d3.select(this).attr("data-country");
+        const visibleLine = d3.select(`.visible-line[data-country="${country}"]`);
+        
+        visibleLine.style("stroke-width", 1);
+    }
+
+});
+}
+
+// Finally, use the functions:
+d3.csv("confirmed.csv").then(data => {
+    const { parsedData, dataByCountry } = parseLineChartData(data);
+    renderLineChart({ parsedData, dataByCountry });
+});
 
 svg2 = d3.select("#world-map-container")
     .append("svg")
@@ -127,25 +186,25 @@ svg2 = d3.select("#world-map-container")
     .attr("height", HEIGHT + MARGIN.top + MARGIN.bottom)
     .style("opacity", 0);  // Ensures initial opacity is 0 for the world map
 
-  var projection = d3.geoMercator()
-      .scale(WIDTH / 2 / Math.PI)
-      .translate([WIDTH / 2, HEIGHT / 1.5]);
+var projection = d3.geoMercator()
+    .scale(WIDTH / 2 / Math.PI)
+    .translate([WIDTH / 2, HEIGHT / 1.5]);
 
-  var path = d3.geoPath().projection(projection);
+var path = d3.geoPath().projection(projection);
 
-  const TARGET_DATE = "1/1/21"; // Example date. Adjust as needed.
+const TARGET_DATE = "1/1/21"; // Example date. Adjust as needed.
 
-  // Adjust this line to aggregate based on the specified date
-  var casesByCountry = Array.from(d3.rollup(data, v => d3.sum(v, d => d[TARGET_DATE]), d => d['Country/Region']));
+// Adjust this line to aggregate based on the specified date
+var casesByCountry = Array.from(d3.rollup(data, v => d3.sum(v, d => d[TARGET_DATE]), d => d['Country/Region']));
 
-  var maxVal = d3.max(casesByCountry, function(d) { return d[1]; });
-  colorScale.domain([0, maxVal]);
+var maxVal = d3.max(casesByCountry, function(d) { return d[1]; });
+colorScale.domain([0, maxVal]);
 
-  // Convert casesByCountry to a map for faster lookups and transform keys to uppercase
-  var casesByCountryMap = new Map(casesByCountry.map(d => [d[0].toUpperCase(), d[1]]));
+// Convert casesByCountry to a map for faster lookups and transform keys to uppercase
+var casesByCountryMap = new Map(casesByCountry.map(d => [d[0].toUpperCase(), d[1]]));
 
-  mapDataPromise = d3.json("https://unpkg.com/world-atlas@2/countries-110m.json");
-  mapDataPromise.then(function(data) {
+mapDataPromise = d3.json("https://unpkg.com/world-atlas@2/countries-110m.json");
+mapDataPromise.then(function(data) {
     svg2.selectAll("path")
     .data(topojson.feature(data, data.objects.countries).features)
     .enter()
@@ -222,23 +281,34 @@ svg2 = d3.select("#world-map-container")
     .attr("text-anchor", "end")
     .text(`${MAX_VALUE} Cases~`);
     });
-  
+
+    // Extract the case determination for a given country feature.
+    function getCasesForCountryFeature(d) {
+        if (!d.properties) return null;
+        
+        var countryName = d.properties.name;
+        return casesByCountryMap.get(countryName.toUpperCase());
+    }
+
     function handleMouseOver(event, d) {
-        // The 'd' here should now correctly refer to the datum bound to the country path.
-        //console.log(d);
-    
-        if (d.properties) {
-            var countryName = d.properties.name;
-            var cases = casesByCountryMap.get(countryName.toUpperCase());
-            console.log(cases);
-    
-            if (cases) {
-                updateStyle(event.currentTarget, "stroke-width", 3);  // Use event.currentTarget to reference the current element
-                updateTooltipForMap(event.pageX, event.pageY, countryName, cases);
-            }
+        const cases = getCasesForCountryFeature(d);
+        if (cases) {
+            d3.select(event.currentTarget).style("stroke-width", 3);
+            const content = `<strong>Country: </strong>${d.properties.name}<br>
+                            <strong>Confirmed Cases: </strong>${cases}`;
+            showTooltip(event, content);
         }
-    }    
-  
+    }
+    
+    function handleMouseOut(event, d) {
+        d3.select(event.currentTarget).style("stroke-width", 1);
+        hideTooltip();
+    }
+
+    function updateStyle(element, styleProperty, value) {
+        d3.select(element).style(styleProperty, value);
+    }
+
     function updateTooltipForMap(pageX, pageY, country, cases) {
         d3.select("#tooltip")
             .style("left", pageX + 15 + "px")
@@ -248,62 +318,26 @@ svg2 = d3.select("#world-map-container")
         d3.select("#tooltip").style("display", "block");
     }
 
-    function handleMouseOut(d, i) {
-        //console.log("mouse out")
-        updateStyle(this, "stroke-width", 1);  // Reset the stroke-width on mouse out
-        hideTooltip();
-    }
-
-    function updateStyle(element, styleProperty, value) {
-        element.style[styleProperty] = value;
-    }
-
     function updateTooltip(pageX, pageY, date, cases, country) {
         d3.select("#tooltip")
             .style("left", pageX + 15 + "px")
             .style("top", pageY + "px")
             .html("<strong>Country: </strong>" + country + "<br>" +
-                  "<strong>Date: </strong>" + date + "<br>" +  // Using 'date' instead of 'countryName' for clarity here
-                  "<strong>Daily Confirmed Cases: </strong>" + cases);
+                "<strong>Date: </strong>" + date + "<br>" +  // Using 'date' instead of 'countryName' for clarity here
+                "<strong>Daily Confirmed Cases: </strong>" + cases);
         d3.select("#tooltip").style("display", "block");
     }
 
+    function showTooltip(event, content) {
+        d3.select("#tooltip")
+            .style("left", event.pageX + 15 + "px")
+            .style("top", event.pageY + "px")
+            .html(content);
+        d3.select("#tooltip").style("display", "block");
+    }
+    
     function hideTooltip() {
         d3.select("#tooltip").style("display", "none");
-    }
-    
-      function handleLineMouseMove(event, d, country) {
-        const [xPos] = d3.pointer(event, this); // Use event here, not d3.event
-        const xDomain = x.invert(xPos);
-        const bisect = d3.bisector(d => d.date).left;
-        const index = bisect(parsedData, xDomain, 1);
-        const a = parsedData[index - 2];
-        //console.log(a)
-        const b = parsedData[index - 1];
-        //console.log(b)
-        const point = xDomain - a.date > b.date - xDomain ? b : a;
-    
-        // Format the date using d3.timeFormat
-        const formattedDate = d3.timeFormat("%Y-%m-%d")(point.date);
-        
-        updateTooltip(event.pageX, event.pageY, formattedDate, point.cases, country);
-    }
-    
-    function handleLineMouseOver(d, i) {
-        const country = d3.select(this).attr("data-country");
-        const visibleLine = d3.select(`.visible-line[data-country="${country}"]`);
-    
-        visibleLine.style("cursor", "pointer");
-        visibleLine.style("stroke-width", 3);
-    }
-    
-    function handleLineMouseOut(d, i) {
-        hideTooltip();
-        
-        const country = d3.select(this).attr("data-country");
-        const visibleLine = d3.select(`.visible-line[data-country="${country}"]`);
-        
-        visibleLine.style("stroke-width", 1);
     }
     
 });
